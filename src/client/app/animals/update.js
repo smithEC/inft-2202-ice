788ddc305180bb409969db4e61ce2af5ec6dd20f
smithEC <connor.ec.smith@gmail.com>
2025-01-09 19:50:33 -0500

    Added Folders and File setup

---
+++ src/client/app/animals/update.js
@@ -0,0 +1,7 @@
+/*
+    Name: Connor Smith
+    filename: aniaml.service.js
+    Course: INFT 2202
+    Date: January 9, 2025
+    Description: This is the update script
+*/
---
+++ src/client/app/app.js
@@ -0,0 +1,7 @@
+/*
+    Name: Connor Smith
+    filename: app.js
+    Course: INFT 2202
+    Date: January 9, 2025
+    Description: This is my general application script.  Functions that are required on every page should live here.
+*/
