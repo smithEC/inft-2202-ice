b3c7a14a9a46632c7c4b6387fd7326a6a8b3070f
smithEC <connor.ec.smith@gmail.com>
2025-01-14 23:06:09 -0500

    Commit for ICE3

--- src/client/app/animals/create.js
+++ src/client/app/animals/create.js
@@ -5,3 +5,118 @@
     Date: January 9, 2025
     Description: This is the create script
 */
+// Get the form
+document.getElementById("animal-form")
+    .addEventListener("submit", submitAnimalForm);
+
+/*
+ * Reveive the submit event from the form
+ */
+
+function submitAnimalForm(event) {
+    event.preventDefault();
+    const animalForm = event.target;
+    console.log(event);
+
+    const eleMessageBox = document.getElementById("message-box");
+    const valid = validateAnimalForm(event.target);
+
+    if (valid) {
+        const animalObject = {
+            name: animalForm.name.value,
+            breed: animalForm.breed.value,
+            legs: animalForm.legs.value,
+            eyes: animalForm.eyes.value,
+            sound: animalForm.sound.value
+        }
+
+        console.log(animalObject);
+        animalForm.reset();
+        eleMessageBox.classList.add("d-none");
+    } else {
+        
+        eleMessageBox.classList.remove("d-none");
+        eleMessageBox.textContent = "Womp womp";
+    }
+}
+
+/*
+ * Check whether or not the form is valid
+ */
+
+function validateAnimalForm(form) {
+    console.log(form);
+    let valid = true;
+
+    // Name validation
+    const name = form.name.value;
+    const eleNameError = form.name.nextElementSibling;
+
+    if (name == "") {
+        valid = false;
+        eleNameError.classList.remove("d-none");
+        eleNameError.textContent = "You must name this animal!";
+    } else {
+        eleNameError.classList.add("d-none");
+    }
+
+    // Breed validation
+    const breed = form.breed.value;
+    const eleBreedError = form.breed.nextElementSibling;
+
+    if (breed == "") {
+        valid = false;
+        eleBreedError.classList.remove("d-none");
+        eleBreedError.textContent = "You must enter the breed!";
+    } else {
+        eleBreedError.classList.add("d-none");
+    }
+
+    // Eyes validation
+    const eyes = form.eyes.value;
+    const eleEyesError = form.eyes.nextElementSibling;
+
+    if (eyes == "") {
+        valid = false;
+        eleEyesError.classList.remove("d-none");
+        eleEyesError.textContent = "Must enter eye count!";
+    } else if (isNaN(eyes)) {
+        valid = false;
+        eleEyesError.classList.remove("d-none");
+        eleEyesError.textContent = "Must be a number";
+    }
+     else {
+        eleEyesError.classList.add("d-none");
+    }
+
+    // Legs validation
+    const legs = form.legs.value;
+    const eleLegsError = form.legs.nextElementSibling;
+
+    if (legs == "") {
+        valid = false;
+        eleLegsError.classList.remove("d-none");
+        eleLegsError.textContent = "Must enter leg count!";
+    } else if (isNaN(legs)) {
+        valid = false;
+        eleLegsError.classList.remove("d-none");
+        eleLegsError.textContent = "Must be a number";
+    }
+     else {
+        eleLegsError.classList.add("d-none");
+    }
+
+    // Sound validation
+    const sound = form.sound.value;
+    const eleSoundError = form.sound.nextElementSibling;
+
+    if (sound == "") {
+        valid = false;
+        eleSoundError.classList.remove("d-none");
+        eleSoundError.textContent = "You must enter a sound!";
+    } else {
+        eleSoundError.classList.add("d-none");
+    }
+
+    return valid;
+}
--- src/client/app/app.js
+++ src/client/app/app.js
@@ -5,3 +5,9 @@
     Date: January 9, 2025
     Description: This is my general application script.  Functions that are required on every page should live here.
 */
+
+const yearElement = document.getElementById('copyright');
+const currentYearText = document.createTextNode(new Date().getFullYear());
+
+
+yearElement.appendChild(currentYearText);
